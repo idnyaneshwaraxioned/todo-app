10770376789015bcb6207b19205bf8451ad4130b
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-06-17 16:26:50 +0530

    Update item done and Css done

--- src/Todo.js
+++ src/Todo.js
@@ -3,12 +3,24 @@ import React, { useState } from 'react';
 const Todo = () => {
 	const [inputData, setInputData] = useState('');
 	const [items, setItems] = useState([{ id: 1, task: "Coding" },]);
+	const [editIndex, setEditIndex] = useState();
+	const [btnflag, setbtnflag] = useState(true);
 
 	// const addItem = () => {
 	// 	const count = items.length;
 	// 	setItems([...items, { id: count + 1, task:inputData }]);
 	// 	setInputData('');
 	// }
+
+	const addData = ()=>{
+		if (inputData) {
+			const count = items.length;
+			setItems([...items, { id: count + 1, task: inputData }])
+			setInputData('');
+		} else {
+			alert("Please enter todos");
+		}
+	}
 
 	const deleteItem = (val) => {
 		console.log(val)
@@ -17,30 +29,51 @@ const Todo = () => {
 		})
 		setItems(filterData);
 	}
+
+	const editItem = (val) => {
+		let findItem = items.find((ele) => {
+			return ele.id === val + 1;
+		})
+		setInputData(findItem.task);
+		setEditIndex(val+1);
+		setbtnflag(false);
+		console.log('editIndex ' + editIndex);
+	}
+
+	const updateItem = ()=>{
+		setItems(
+			items.map((ele)=>{
+				if(ele.id===editIndex){
+					return{...ele,task:inputData}
+				}
+				return ele;
+			})
+		)
+		setInputData('');
+		setbtnflag(true);
+	}
+
 	return (
 		<div className="container">
 			<div className="wrapper">
 				<h1>React To-Do</h1>
 				<div className="todo-form">
 					<input type="text" placeholder="Enter your todo" value={inputData} onChange={(e) => setInputData(e.target.value)} />
-					<button onClick={
-						() => {
-							if(inputData){
-								const count = items.length;
-								setItems([...items, { id: count + 1, task:inputData }])
-								setInputData('');
-							} else {
-								alert("Please enter todos");
-							}
-						}
-					} >ADD</button>
+					{btnflag ? <button onClick={addData} className="addbtn">ADD</button>:
+					<button className="updatebtn" onClick={updateItem}>Update</button>}
 				</div>
 				<div className="todo-item">
 					<ul className="todos">
 						{
 							items.map((elem, index) => {
 								return (
-								<li className="todo" key={elem.id}><span>{elem.task}</span><button className="del" onClick={() => deleteItem(index)}>Del</button></li>
+									<li className="todo" key={elem.id}>
+										<span>{elem.task}</span>
+										<div>
+											<button className="del" onClick={() => deleteItem(index)}>Del</button>
+											<button className="del" onClick={() => editItem(index)}>Edit</button>
+										</div>
+									</li>
 								)
 							})
 						}
